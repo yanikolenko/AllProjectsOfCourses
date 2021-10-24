fd9853ab903f41e551d0e02756ca72de66ab9242
Yaroslav Nikolenko <nikolenkoyarik0@gmail.com>
2021-10-24 21:55:40 +0300

    FirstHomework

---
+++ p52project/src/main/java/com/yaroslavnikolenko/secondscreencourses/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.yaroslavnikolenko.secondscreencourses
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner1: Spinner = findViewById(R.id.spinner1)
+        val spinner2: Spinner = findViewById(R.id.spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.email_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner2.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.phone_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner1.adapter = adapter
+        }
+
+    }
+}
